0ea7ce654a8c2031cfe92f4f2c0a9f96fa318c55
Jörgen Bygdemark <jorgenbygdemark@gmail.com>
2022-08-17 16:41:51 +0200

    updated paths

--- src/App.js
+++ src/App.js
@@ -1,167 +1,170 @@
 import React, { Component } from 'react';
 import Container from 'react-bootstrap/Container';
 import Stack from 'react-bootstrap/Stack';
-import SearchBar from './SearchBar';
-import ImageList from './ImageList';
-import unsplash from '../api/unsplash';
-import ImageDetailCard from './ImageDetailCard';
-import ListPagination from './ListPagination';
+import SearchBar from './components/SearchBar';
+import ImageList from './components/ImageList';
+import unsplash from './api/unsplash';
+import ImageDetailCard from './components/ImageDetailCard';
+import ListPagination from './components/ListPagination';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import '../css/custom.css';
+import './css/custom.css';
 
 class App extends Component {
-    state = { 
-        images: [], 
-        term: '', 
-        imagesFound: true, 
-        pageNum: 1, 
-        totalPages: 0, 
-        totalitems: 0,
-        hasSelectedImage: false,
-        selectedImage: null };
+  state = {
+    images: [],
+    term: '',
+    imagesFound: true,
+    pageNum: 1,
+    totalPages: 0,
+    totalitems: 0,
+    hasSelectedImage: false,
+    selectedImage: null,
+  };
 
-    componentDidMount() {
-        this.getRandomPhotosOnLoad();
+  componentDidMount() {
+    this.getRandomPhotosOnLoad();
+  }
+
+  getRandomPhotosOnLoad = async () => {
+    const response = await unsplash.get('/photos/random', {
+      params: { count: 10 },
+    });
+
+    console.log(response);
+
+    this.setState({
+      ...this.state,
+      pageNum: 1,
+      images: response.data,
+      imagesFound: true,
+    });
+    if (response.data.length === 0) {
+      this.setState({ imagesFound: false });
     }
+  };
 
-    getRandomPhotosOnLoad = async () => {
-        const response = await unsplash.get('/photos/random', {
-            params: { count: 10}
-        });
+  onSearchSubmit = async (term) => {
+    const response = await unsplash.get('/search/photos', {
+      params: { query: term },
+    });
 
-        console.log(response);
-
-        this.setState({
-            ...this.state,
-            pageNum: 1,
-            images: response.data,
-            imagesFound: true,
-        });
-        if (response.data.length === 0) {
-            this.setState({ imagesFound: false });
-        }
+    this.setState({
+      term: term,
+      pageNum: 1,
+      images: response.data.results,
+      imagesFound: true,
+      totalPages: response.data.total_pages,
+      totalitems: response.data.total,
+      hasSelectedImage: false,
+      selectedImage: null,
+    });
+    if (response.data.results.length === 0) {
+      this.setState({ imagesFound: false });
     }
+  };
 
-    onSearchSubmit = async (term) => {
-        const response = await unsplash.get('/search/photos', {
-            params: { query: term }
-        });
+  onPageChange = async (pageNum) => {
+    const response = await unsplash.get('/search/photos', {
+      params: { query: this.state.term, page: pageNum },
+    });
 
-        this.setState({
-            term: term,
-            pageNum: 1,
-            images: response.data.results,
-            imagesFound: true,
-            totalPages: response.data.total_pages,
-            totalitems: response.data.total,
-            hasSelectedImage: false,
-            selectedImage: null
-        });
-        if (response.data.results.length === 0) {
-            this.setState({ imagesFound: false });
-        }
+    this.setState({
+      ...this.state,
+      pageNum: pageNum,
+      images: response.data.results,
+      imagesFound: true,
+      totalPages: response.data.total_pages,
+      totalitems: response.data.total,
+      hasSelectedImage: false,
+      selectedImage: null,
+    });
+    if (response.data.results.length === 0) {
+      this.setState({ imagesFound: false });
     }
+    window.scrollTo(0, 0);
+  };
 
-    onPageChange = async (pageNum) => {
-        const response = await unsplash.get('/search/photos', {
-            params: { query: this.state.term, page: pageNum }
-        });
+  onImageSelect = (image) => {
+    this.setState({
+      ...this.state,
+      hasSelectedImage: true,
+      selectedImage: image,
+    });
+    window.scrollTo(0, 0);
+  };
 
-        this.setState({
-            ...this.state,
-            pageNum: pageNum,
-            images: response.data.results,
-            imagesFound: true,
-            totalPages: response.data.total_pages,
-            totalitems: response.data.total,
-            hasSelectedImage: false,
-            selectedImage: null
-        });
-        if (response.data.results.length === 0) {
-            this.setState({ imagesFound: false });
-        }
-        window.scrollTo(0, 0);
+  onCloseImage = () => {
+    this.setState({
+      ...this.state,
+      hasSelectedImage: false,
+      selectedImage: null,
+    });
+  };
+
+  onImageDownload = () => {
+    console.log('trigger download');
+  };
+
+  renderSelectedImage = () => {
+    if (this.state.hasSelectedImage) {
+      return (
+        <React.Fragment>
+          <Row>
+            <Col sm={4}>
+              <ImageDetailCard
+                image={this.state.selectedImage}
+                onCloseImage={this.onCloseImage}
+                onImageDownload={this.onImageDownload}
+              />
+            </Col>
+            <Col sm={8}>
+              <ImageList
+                images={this.state.images}
+                renderResults={this.state.imagesFound}
+                onImageSelect={this.onImageSelect}
+              />
+              <ListPagination
+                totPages={this.state.totalPages}
+                currentPage={this.state.pageNum}
+                pageClicked={(ele) => {
+                  this.onPageChange(ele);
+                }}
+              />
+            </Col>
+          </Row>
+        </React.Fragment>
+      );
+    } else {
+      return (
+        <React.Fragment>
+          <ImageList
+            images={this.state.images}
+            renderResults={this.state.imagesFound}
+            onImageSelect={this.onImageSelect}
+          />
+          <ListPagination
+            totPages={this.state.totalPages}
+            currentPage={this.state.pageNum}
+            pageClicked={(ele) => {
+              this.onPageChange(ele);
+            }}
+          />
+        </React.Fragment>
+      );
     }
+  };
 
-    onImageSelect = (image) => {
-        this.setState({
-            ...this.state,
-            hasSelectedImage: true,
-            selectedImage: image
-        });
-        window.scrollTo(0, 0);
-    }
-
-    onCloseImage = () => {
-        this.setState({
-            ...this.state,
-            hasSelectedImage: false,
-            selectedImage: null
-        })
-    }
-
-    onImageDownload = () => {
-        console.log("trigger download");
-    }
-
-    renderSelectedImage = () => {
-        if (this.state.hasSelectedImage) {
-            return (
-                <React.Fragment>
-                    <Row>
-                        <Col sm={4}>
-                                <ImageDetailCard 
-                                    image={this.state.selectedImage} 
-                                    onCloseImage={this.onCloseImage}
-                                    onImageDownload={this.onImageDownload}
-                                />
-                        </Col>
-                        <Col sm={8}>
-                            <ImageList
-                                images={this.state.images}
-                                renderResults={this.state.imagesFound}
-                                onImageSelect={this.onImageSelect}
-                            />
-                            <ListPagination
-                                totPages={this.state.totalPages}
-                                currentPage={this.state.pageNum}
-                                pageClicked={(ele) => { this.onPageChange(ele) }}
-                            />
-                        </Col>
-                    </Row>
-
-                </React.Fragment>
-            )
-        }
-        else {
-            return (
-                <React.Fragment>
-                    <ImageList
-                        images={this.state.images}
-                        renderResults={this.state.imagesFound}
-                        onImageSelect={this.onImageSelect}
-                    />
-                    <ListPagination
-                    totPages={this.state.totalPages}
-                    currentPage={this.state.pageNum}
-                    pageClicked={(ele) => { this.onPageChange(ele) }}
-                    />
-                </React.Fragment>
-            );
-        }
-    }
-
-    render() {
-        return (
-            <Container fixed="realtive">
-                <SearchBar onSubmit={this.onSearchSubmit} />
-                    <Stack gap={3} className="mx-auto pt-2">
-                            {this.renderSelectedImage()}
-                    </Stack>
-            </Container>
-        );
-    }
+  render() {
+    return (
+      <Container fixed='realtive'>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+        <Stack gap={3} className='mx-auto pt-2'>
+          {this.renderSelectedImage()}
+        </Stack>
+      </Container>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
+import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
-ReactDOM.render(<App />, 
-    document.querySelector('#root'))
+ReactDOM.render(<App />, document.querySelector('#root'));
